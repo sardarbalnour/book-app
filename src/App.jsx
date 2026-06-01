348f58b2e768661a83f8d738a82d5fce9e7fb26a
Sardar balnour <balnour.sardar@gmail.com>
2026-06-01 12:12:02 +0330

    BookCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { books } from "./constants/mockData";
+import Books from "./components/Books";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <Layout>
-      <h1>Book App Starting files</h1>
-      <p>{books.map((book) => book.title)}</p>
+      <Books />
     </Layout>
   );
 }
