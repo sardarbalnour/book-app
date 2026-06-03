154d5110cea52b04a553771e14691f4972dad019
Sardar balnour <balnour.sardar@gmail.com>
2026-06-04 01:23:48 +0330

    SideCard and styles

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import { books } from "../constants/mockData";
 import BookCard from "./BookCard";
+import SideCard from "./SideCard";
+
+import styles from "./Books.module.css";
 
 function Books() {
   const [liked, setLiked] = useState([]);
@@ -16,8 +19,8 @@ function Books() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cards}>
         {books.map((book) => (
           <BookCard
             key={book.id}
@@ -26,7 +29,14 @@ function Books() {
           />
         ))}
       </div>
-      {!!liked.length && <div>ss</div>}
+      {!!liked.length && (
+        <div className={styles.favorite}>
+          <h4>Favorite</h4>
+          {liked.map((book) => (
+            <SideCard key={book.id} data={book} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
